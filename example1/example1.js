180caaba3b5727881d0313fab6a640d38492e73a
Jeffrey Bush <jeff@coderforlfie.com>
2018-08-07 16:55:23 -0400

    Initial commit.
    Common code, first example, and a script to run a local server using Twisted.

---
+++ example1/example1.js
@@ -0,0 +1,74 @@
+// This is a very simple WebGL example that draws a triangle. It depends on the common code given.
+// NOTE: although technically code can be at the top-scope, in general you want all code to be in
+// a function (one exception is setting up the 'load' event as below).
+
+// Global WebGL context variable
+var gl;
+
+// Once the document is fully loaded run this init function.
+window.addEventListener('load', function init()
+{
+	// Get the HTML5 canvas object from it's ID
+	var canvas = document.getElementById("gl-canvas");
+
+	// Get the WebGL context (save into a global variable)
+	gl = WebGLUtils.create3DContext(canvas);
+	if (!gl)
+	{
+		alert("WebGL isn't available");
+		return;
+	}
+
+	// Configure WebGL
+	gl.viewport(0, 0, canvas.width, canvas.height); // this is the region of the canvas we want to draw on (all of it)
+	gl.clearColor(1.0, 1.0, 1.0, 0.0); // setup the background color with red, green, blue, and alpha
+
+	// The vertices for the triangle (uses 2D vectors)
+	var verts = [
+		vec2(-1, -1),
+		vec2( 0,  1),
+		vec2( 1, -1),
+	];
+
+	// Compile shaders
+	// Vertex Shader: simplest possible
+	var vertShdr = compileShader(gl, gl.VERTEX_SHADER, `
+		attribute vec4 vPosition;
+		void main()
+		{
+			gl_Position = vPosition;
+		}
+	`);
+	// Fragment Shader: simplest possible, chosen color is red for each point
+	var fragShdr = compileShader(gl, gl.FRAGMENT_SHADER, `
+		precision mediump float;
+		void main()
+		{
+			gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+		}
+	`);
+
+	// Link the programs and use them with the WebGL context
+	var program = linkProgram(gl, [vertShdr, fragShdr]);
+	gl.useProgram(program);
+
+	// Load the vertex data into the GPU and associate with shader
+	var bufferId = gl.createBuffer(); // create a new buffer
+	gl.bindBuffer(gl.ARRAY_BUFFER, bufferId); // bind to the new buffer
+	gl.bufferData(gl.ARRAY_BUFFER, flatten(verts), gl.STATIC_DRAW); // load the flattened data into the buffer
+	var vPosition = gl.getAttribLocation(program, "vPosition"); // get the vertex shader attribute "vPosition"
+	gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, false, 0, 0); // associate the buffer with "vPosition" making sure it knows it is length-2 vectors of floats
+	gl.enableVertexAttribArray(vPosition); // enable this set of data
+
+	// Render the static scene
+	render();
+});
+
+/**
+ * Render the scene. Currently just a simple static triangle with 3 points loaded in init.
+ */
+function render()
+{
+	gl.clear(gl.COLOR_BUFFER_BIT);
+	gl.drawArrays(gl.TRIANGLES, 0, 3);
+}
