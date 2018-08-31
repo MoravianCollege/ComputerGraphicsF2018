cbd62fa421c44edeb90c6c68164257f3543d7745
Jeffrey Bush <jeff@coderforlfie.com>
2018-08-31 09:10:45 -0400

    Fixing linter messages in init-shaders.

--- common/init-shaders.js
+++ common/init-shaders.js
@@ -3,10 +3,9 @@
  * file asynchronously. Once the file is loaded the `ondone` function is called. If the file fails
  * to load the `onerror` function is called.
  */
-function loadFile(name, ondone, onerror)
-{
-	var xhr = new XMLHttpRequest();
-    xhr.addEventListener('load', ondone);
+function loadFile(name, ondone, onerror) {
+	let xhr = new XMLHttpRequest();
+	xhr.addEventListener('load', ondone);
 	xhr.addEventListener('error', onerror);
 	xhr.open('GET', name, true);
 	xhr.send(null);
@@ -16,13 +15,11 @@ function loadFile(name, ondone, onerror)
  * Compiles and checks the shader. The type must be one of the OpenGL constants for shaders like
  * gl.VERTEX_SHADER or gl.FRAGMENT_SHADER.
  */
-function compileShader(gl, type, shader)
-{
-	var shdr = gl.createShader(type);
+function compileShader(gl, type, shader) {
+	let shdr = gl.createShader(type);
 	gl.shaderSource(shdr, shader);
 	gl.compileShader(shdr);
-	if (!gl.getShaderParameter(shdr, gl.COMPILE_STATUS))
-	{
+	if (!gl.getShaderParameter(shdr, gl.COMPILE_STATUS)) {
 		throw "Shader failed to compile. The error log is:<pre>" + gl.getShaderInfoLog(shdr) + "</pre>";
 	}
 	return shdr;
@@ -31,16 +28,13 @@ function compileShader(gl, type, shader)
 /**
  * Links multiple shaders into a program.
  */
-function linkProgram(gl, shaders)
-{
-	var program = gl.createProgram();
-	for (var i = 0; i < shaders.length; ++i)
-	{
+function linkProgram(gl, shaders) {
+	let program = gl.createProgram();
+	for (let i = 0; i < shaders.length; ++i) {
 		gl.attachShader(program, shaders[i]);
 	}
 	gl.linkProgram(program);
-	if (!gl.getProgramParameter(program, gl.LINK_STATUS))
-	{
+	if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
 		throw "Shader program failed to link. The error log is:<pre>" + gl.getProgramInfoLog(program) + "</pre>";
 	}
 	return program;
