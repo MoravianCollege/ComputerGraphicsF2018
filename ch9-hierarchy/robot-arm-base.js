7b3d630af6531e966e4d891ddb3aeec45f31af10
bushj <bushj@moravian.edu>
2018-12-03 13:09:14 -0500

    Adding robot arm base demo.

---
+++ ch9-hierarchy/robot-arm-base.js
@@ -0,0 +1,200 @@
+// This is a WebGL example that demonstrates basic hierarchial modeling with
+// a robot arm.
+
+// Global WebGL context variable
+let gl;
+
+// Global uniform locations
+let model_view_loc;
+
+// Description of model
+const BASE_HEIGHT = 0.1;
+const ARM1_LENGTH = 0.4;
+const ARM2_LENGTH = 0.2;
+let base = {
+	// Robot Arm base - if the base rotates then everything rotates
+	'position': vec3(0,0,0), 'rotation': vec3(0,45,0),
+	'start': 0, 'count': 0,
+};
+let lower = {
+	// First part of the robot arm
+	'position': vec3(0,BASE_HEIGHT,0), 'rotation': vec3(0,0,45),
+	'start': 0, 'count': 0,
+};
+let upper = {
+	// Second part of the robot arm
+	'position': vec3(0,ARM1_LENGTH,0), 'rotation': vec3(0,0,45),
+	'start': 0, 'count': 0,
+};
+
+window.addEventListener('load', function init() {
+	// Get the HTML5 canvas object from it's ID
+	const canvas = document.getElementById('gl-canvas');
+
+	// Get the WebGL context (save into a global variable)
+	gl = WebGLUtils.create3DContext(canvas, {premultipliedAlpha:false});
+	if (!gl) {
+		window.alert("WebGL isn't available");
+		return;
+	}
+
+	// Configure WebGL
+	gl.getExtension('OES_standard_derivatives');
+	gl.clearColor(0.0, 0.0, 0.0, 0.0); // setup the background color with red, green, blue, and alpha
+	gl.enable(gl.DEPTH_TEST); // things further away will be hidden
+	gl.enable(gl.CULL_FACE);
+	gl.cullFace(gl.FRONT);
+	onResize();
+
+	// Create cubes to use for all parts
+	let verts = [], inds = [];
+	base.start = inds.length;
+	cube(
+		vec3(-0.1, 0.0, -0.1), vec3(0.1, 0.0, -0.1), vec3(0.1, BASE_HEIGHT, -0.1), vec3(-0.1, BASE_HEIGHT, -0.1),
+		vec3(-0.1, BASE_HEIGHT,  0.1), vec3(0.1, BASE_HEIGHT,  0.1), vec3(0.1, 0.0,  0.1), vec3(-0.1, 0.0,  0.1),
+		verts, inds);
+	base.count = inds.length - base.start;
+
+	lower.start = inds.length;
+	cube(
+		vec3(-0.01, 0.0, -0.01), vec3(0.01, 0.0, -0.01), vec3(0.01, ARM1_LENGTH, -0.01), vec3(-0.01, ARM1_LENGTH, -0.01),
+		vec3(-0.01, ARM1_LENGTH,  0.01), vec3(0.01, ARM1_LENGTH,  0.01), vec3(0.01, 0.0,  0.01), vec3(-0.01, 0.0,  0.01),
+		verts, inds);
+	lower.count = inds.length - lower.start;
+
+	upper.start = inds.length;
+	cube(
+		vec3(-0.01, 0.0, -0.01), vec3(0.01, 0.0, -0.01), vec3(0.01, ARM2_LENGTH, -0.01), vec3(-0.01, ARM2_LENGTH, -0.01),
+		vec3(-0.01, ARM2_LENGTH,  0.01), vec3(0.01, ARM2_LENGTH,  0.01), vec3(0.01, 0.0,  0.01), vec3(-0.01, 0.0,  0.01),
+		verts, inds);
+	upper.count = inds.length - upper.start;
+
+	// Compile shaders
+	let vertShdr = compileShader(gl, gl.VERTEX_SHADER, `
+		attribute vec4 vPosition;
+		uniform mat4 model_view;
+		uniform mat4 projection;
+		varying vec3 pos, L, V;
+		void main() {
+			gl_Position = projection*model_view*vPosition;
+			pos = mat3(model_view)*vPosition.xyz;
+			L = normalize(vec3(0.0, 1.0, -1.0)-pos);
+			V = normalize(vec3(0.0, 0.0, 0.0)-pos);
+		}
+	`);
+	let fragShdr = compileShader(gl, gl.FRAGMENT_SHADER, `
+		#extension GL_OES_standard_derivatives : enable
+		precision mediump float;
+		const vec3 color = vec3(0.6, 0.6, 0.6);
+		const float ka = 0.5, kd = 0.7, ks = 0.5, shininess = 2000.0;
+		varying vec3 pos, L, V;
+		void main() {
+			vec3 n = normalize(cross(dFdy(pos), dFdx(pos)));
+			vec3 l = -normalize(L);
+			vec3 v = normalize(V);
+			vec3 h = normalize(l + v);
+			float d = max(dot(l, n), 0.0);
+			float s = (d == 0.0) ? 0.0 : pow(max(dot(n, h), 0.0), shininess);
+			gl_FragColor.rgb = (ka + kd*d)*color + ks*s*vec3(1.0, 1.0, 1.0);
+			gl_FragColor.a = 1.0; // force opaque
+		}
+	`);
+
+	// Link the programs
+	let program = linkProgram(gl, [vertShdr, fragShdr]);
+	gl.useProgram(program);
+
+	// Get the uniforms
+	model_view_loc = gl.getUniformLocation(program, 'model_view');
+
+	// Load the vertex data into the GPU
+	create_vertex_attr_buffer(gl, program, 'vPosition', verts);
+	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, gl.createBuffer());
+	gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(inds), gl.STATIC_DRAW);
+
+	// Add projection to the view
+	add_standard_handlers(program);
+
+	// Listen to sliders for the pieces
+	document.getElementById('base-angle').addEventListener('change', function () {
+		base.rotation[1] = +this.value;
+	});
+	document.getElementById('lower-angle').addEventListener('change', function () {
+		lower.rotation[2] = +this.value;
+	});
+	document.getElementById('upper-angle').addEventListener('change', function () {
+		upper.rotation[2] = +this.value;
+	});
+
+	// Render the scene
+	render();
+});
+
+/**
+ * Render the scene.
+ */
+function render() {
+	// Clear
+	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+	// Draw each part
+	// The 3 parts are in base, lower, and upper.
+	// Each part is drawn with drawElements from .start with .count vertices
+	// Each part will need to incorporate its position and rotation into the
+	// model-view matrix being built then send the updated mv matrix to the GPU.
+	// The model-view matrix is not reset between parts.
+	let mv = mat4(); // start with no transformation
+	// Drawing Base
+	mv = mult(mv, translate(base.position[0], base.position[1], base.position[2]));
+	mv = mult(mv, rotateZ(base.rotation[2]));
+	mv = mult(mv, rotateY(base.rotation[1]));
+	mv = mult(mv, rotateX(base.rotation[0]));
+	gl.uniformMatrix4fv(model_view_loc, false, flatten(mv));
+	gl.drawElements(gl.TRIANGLE_STRIP, base.count, gl.UNSIGNED_SHORT, 2*base.start);
+	// TODO: Lower Arm
+	// TODO: Upper Arm
+
+	// Animate
+	window.requestAnimationFrame(render);
+}
+
+
+//////////////////////// Standard Movement Handlers ////////////////////////
+// Uniform locations
+let projection_loc;
+
+/**
+ * Sets up our standard movement handlerrs for the given program.
+ */
+function add_standard_handlers(program) {
+	// Get the uniforms
+	projection_loc = gl.getUniformLocation(program, 'projection');
+
+	// Give the uniforms their initial values
+	update_projection();
+
+	// Add the resize listener
+	window.addEventListener('resize', onResize);
+}
+
+/**
+ * Updates the projection transformation based on the global variables.
+ */
+function update_projection() {
+	let p, w = gl.canvas.width, h = gl.canvas.height;
+	p = perspective(45, w/h, 0.01, 10);
+	// Need to move the camera away from the origin
+	p = mult(p, translate(0, -0.25, -2));
+	gl.uniformMatrix4fv(projection_loc, false, flatten(p));
+}
+
+/**
+ * Make the canvas fit the window
+ */
+function onResize() {
+	let w = window.innerWidth, h = window.innerHeight;
+	gl.canvas.width = w;
+	gl.canvas.height = h;
+	gl.viewport(0, 0, w, h);
+	update_projection();
+}
---
+++ ch9-hierarchy/tools.js
@@ -0,0 +1,384 @@
+// Various useful functions
+/* exported get_image_data */
+/* exported create_vertex_attr_buffer load_texture load_cubemap_texture */
+/* exported create_buffer, enable_attribute, set_vertex_attr_buffer */
+/* exported create_texture render_to_texture render_to_screen */
+/* exported calc_normals generate_mesh */
+/* exported cube tetrahedron unit_sphere sphere */
+
+/**
+ * Gets the pixel data from a texture.
+ */
+function get_image_data(gl, texture) {
+	// Create a framebuffer backed by the texture
+	let framebuffer = gl.createFramebuffer();
+	gl.bindFramebuffer(gl.FRAMEBUFFER, framebuffer);
+	gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, texture, 0);
+
+	// Read the contents of the framebuffer (data stores the pixel data)
+	let data = new Uint8Array(texture.width * texture.height * 4);
+	gl.readPixels(0, 0, texture.width, texture.height, gl.RGBA, gl.UNSIGNED_BYTE, data);
+	gl.deleteFramebuffer(framebuffer);
+	return data;
+}
+
+/**
+ * Creates a vertex attribute buffer for the given program and attribute with
+ * the given name. If x is an array, it is used as the initial values in the
+ * buffer. Otherwise it must be an integer and specifies the size of the buffer.
+ * In addition, if x is not an array, n must be provided which is the dimension
+ * of the data to be allocated eventually.
+ */
+function create_vertex_attr_buffer(gl, program, name, x, n) {
+	let is_array = Array.isArray(x);
+	let bufferId = gl.createBuffer(); // create a new buffer
+	gl.bindBuffer(gl.ARRAY_BUFFER, bufferId); // bind to the new buffer
+	gl.bufferData(gl.ARRAY_BUFFER, is_array ? flatten(x) : (x*n*sizeof.vec2/2), gl.STATIC_DRAW); // load the flattened data into the buffer
+	let attrib_loc = gl.getAttribLocation(program, name); // get the vertex shader attribute location
+	gl.vertexAttribPointer(attrib_loc, is_array ? x[0].length : n, gl.FLOAT, false, 0, 0); // associate the buffer with the attributes making sure it knows its type
+	gl.enableVertexAttribArray(attrib_loc); // enable this set of data
+	return bufferId;
+}
+
+/**
+ * Creates a buffer with the given data. If x is an array, it is used as the
+ * initial values in the buffer. Otherwise it must be an integer and specifies
+ * the size of the buffer. In addition, if x is not an array, n must be provided
+ * which is the dimension of the data to be allocated eventually.
+ */
+function create_buffer(gl, x, n) {
+	let is_array = Array.isArray(x);
+	let buffer_id = gl.createBuffer(); // create a new buffer
+	gl.bindBuffer(gl.ARRAY_BUFFER, buffer_id); // bind to the new buffer
+	gl.bufferData(gl.ARRAY_BUFFER, is_array ? flatten(x) : (x*n*sizeof.vec2/2), gl.STATIC_DRAW); // load the flattened data into the buffer
+	return buffer_id;
+}
+
+/**
+ * Enables a vertex attribute buffer for the given program and attribute with
+ * the given name. Returns the attribute location.
+ */
+function enable_attribute(gl, program, name) {
+	let attrib_loc = gl.getAttribLocation(program, name); // get the vertex shader attribute location
+	gl.enableVertexAttribArray(attrib_loc); // enable this set of data
+	return attrib_loc;
+}
+
+/**
+ * Sets a vertex attribute to point to the given buffer. The number of elements
+ * per value (e.g. 4 for vec4s) is required as well.
+ */
+function set_vertex_attr_buffer(gl, attrib_loc, buffer_id, n) {
+	gl.bindBuffer(gl.ARRAY_BUFFER, buffer_id); // bind to the new buffer
+	gl.vertexAttribPointer(attrib_loc, n, gl.FLOAT, false, 0, 0); // associate the buffer with the attributes making sure it knows its type
+}
+
+/**
+ * Create a blank texture on the GPU. The width and height do not need to be
+ * powers-of-two (the settings will be changed to handle it). The image will be
+ * flipped vertically and will support mipmapping. Optional arguments for index
+ * number to load into (default #0) and magnification filter (default gl.LINEAR).
+ * Returns the texture object. Also, the old texture can be passed in which is
+ * then resized.
+ */
+function create_texture(gl, w, h, idx, mag_filter, old_tex) {
+	if (typeof idx === "undefined") { idx = 0; }
+	if (typeof mag_filter === "undefined") { mag_filter = gl.LINEAR; }
+
+	let have_old = (old_tex !== null && typeof old_tex !== "undefined");
+	let texture = have_old ? old_tex : gl.createTexture(); // create a texture resource on the GPU
+
+	gl.activeTexture(gl['TEXTURE'+idx]); // set the current texture that all following commands will apply to
+	gl.bindTexture(gl.TEXTURE_2D, texture); // assign our texture resource as the current texture
+
+	if (have_old && old_tex.width === w && old_tex.height === h) { return texture; }
+
+	// Create the image data of the texture
+	gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
+	gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, w, h, 0, gl.RGBA, gl.UNSIGNED_BYTE, null);
+
+	// Save the width and height for later
+	texture.width = w;
+	texture.height = h;
+
+	// Setup options for downsampling, upsampling, and wrapping the texture data
+	if ((w && (w & (w - 1)) === 0) && (h && (h & (h - 1)) === 0)) {
+		// Dimensions are powers-of-two
+		gl.generateMipmap(gl.TEXTURE_2D);
+		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST_MIPMAP_LINEAR);
+		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, mag_filter);
+	} else {
+		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+	}
+
+	return texture;
+}
+
+/**
+ * Specifies that future calls to gl.drawX should render to the given frame buffer, render buffer,
+ * and texture. The frame buffer and render buffer need to be created using gl.createFramebuffer()
+ * and gl.createRenderbuffer() respectively. The texture needs to be created with create_texture().
+ */
+function render_to_texture(gl, framebuffer, renderbuffer, texture) {
+	// Bind to the framebuffer and renderbuffer
+	gl.bindFramebuffer(gl.FRAMEBUFFER, framebuffer);
+	gl.bindRenderbuffer(gl.RENDERBUFFER, renderbuffer);
+
+	// Setup the render buffer and frame buffer
+	if (gl.getRenderbufferParameter(gl.RENDERBUFFER, gl.RENDERBUFFER_WIDTH) !== texture.width ||
+		gl.getRenderbufferParameter(gl.RENDERBUFFER, gl.RENDERBUFFER_HEIGHT) !== texture.height) {
+		gl.renderbufferStorage(gl.RENDERBUFFER, gl.DEPTH_COMPONENT16, texture.width, texture.height);
+	}
+	gl.framebufferRenderbuffer(gl.FRAMEBUFFER, gl.DEPTH_ATTACHMENT, gl.RENDERBUFFER, renderbuffer);
+	gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, texture, 0);
+	let status = gl.checkFramebufferStatus(gl.FRAMEBUFFER);
+	if (status !== gl.FRAMEBUFFER_COMPLETE) {
+		throw "Framebuffer failed to complete: "+status;
+	}
+
+	// Make sure we are drawing to the entire texture
+	gl.viewport(0, 0, texture.width, texture.height);
+}
+
+/**
+ * Specifies that future calls to gl.drawX should render to the screen.
+ */
+function render_to_screen(gl) {
+	// Bind to the defaults
+	gl.bindFramebuffer(gl.FRAMEBUFFER, null);
+	gl.bindRenderbuffer(gl.RENDERBUFFER, null);
+
+	// Make sure we are drawing to the entire canvas
+	gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+}
+
+/**
+ * Load a texture onto the GPU. The image must be power-of-two sized image using RGBA with uint8
+ * values. The image will be flipped vertically and will support mipmapping. Optional arguments for
+ * index number to load into (default #0) and magnification filter (default gl.LINEAR). Returns the
+ * texture object.
+ */
+function load_texture(gl, img, idx, mag_filter) {
+	if (typeof idx === "undefined") { idx = 0; }
+	if (typeof mag_filter === "undefined") { mag_filter = gl.LINEAR; }
+
+	let texture = gl.createTexture(); // create a texture resource on the GPU
+	gl.activeTexture(gl['TEXTURE'+idx]); // set the current texture that all following commands will apply to
+	gl.bindTexture(gl.TEXTURE_2D, texture); // assign our texture resource as the current texture
+
+	// Load the image data into the texture
+	gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
+	gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, img);
+
+	// Save the width and height for later
+	texture.width = img.width;
+	texture.height = img.height;
+
+	// Setup options for downsampling and upsampling the image data
+	gl.generateMipmap(gl.TEXTURE_2D);
+	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST_MIPMAP_LINEAR);
+	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, mag_filter);
+
+	return texture;
+}
+
+/**
+ * Load a texture onto the GPU as a cube-map texture. The images must be power-of-two sized image
+ * using RGBA with uint8 values. Optional arguments for index number to load into (default #0) and
+ * magnification filter (default gl.LINEAR). Returns the texture object.
+ */
+function load_cubemap_texture(gl, xp, xn, yp, yn, zp, zn, idx, mag_filter) {
+	if (typeof idx === "undefined") { idx = 0; }
+	if (typeof mag_filter === "undefined") { mag_filter = gl.LINEAR; }
+
+	let texture = gl.createTexture(); // create a texture resource on the GPU
+	gl.activeTexture(gl['TEXTURE'+idx]); // set the current texture that all following commands will apply to
+	gl.bindTexture(gl.TEXTURE_CUBE_MAP, texture); // assign our texture resource as the current texture
+
+	// Load the image data into the texture
+	gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, false);
+	gl.texImage2D(gl.TEXTURE_CUBE_MAP_POSITIVE_X,0,gl.RGBA,gl.RGBA,gl.UNSIGNED_BYTE,xp);
+	gl.texImage2D(gl.TEXTURE_CUBE_MAP_NEGATIVE_X,0,gl.RGBA,gl.RGBA,gl.UNSIGNED_BYTE,xn);
+	gl.texImage2D(gl.TEXTURE_CUBE_MAP_POSITIVE_Y,0,gl.RGBA,gl.RGBA,gl.UNSIGNED_BYTE,yp);
+	gl.texImage2D(gl.TEXTURE_CUBE_MAP_NEGATIVE_Y,0,gl.RGBA,gl.RGBA,gl.UNSIGNED_BYTE,yn);
+	gl.texImage2D(gl.TEXTURE_CUBE_MAP_POSITIVE_Z,0,gl.RGBA,gl.RGBA,gl.UNSIGNED_BYTE,zp);
+	gl.texImage2D(gl.TEXTURE_CUBE_MAP_NEGATIVE_Z,0,gl.RGBA,gl.RGBA,gl.UNSIGNED_BYTE,zn);
+
+	// Setup options for downsampling and upsampling the image data
+	gl.generateMipmap(gl.TEXTURE_CUBE_MAP);
+	gl.texParameteri(gl.TEXTURE_CUBE_MAP, gl.TEXTURE_MIN_FILTER, gl.NEAREST_MIPMAP_LINEAR);
+	gl.texParameteri(gl.TEXTURE_CUBE_MAP, gl.TEXTURE_MAG_FILTER, mag_filter);
+	return texture;
+}
+
+/**
+ * Calculates the normals for the vertices given an array of vertices and array of indices to look
+ * up into. By default this assumes the indices represents a triangle strip. To work with triangles
+ * pass a third argument of false. The optional fourth argument says which triangles (as indices) to
+ * skip during the calculations.
+ */
+function calc_normals(verts, inds, strip, skip) {
+	if (strip !== true && strip !== false) { strip = true; }
+	let normals = new Array(verts.length);
+
+	// Setup skip information
+	skip = typeof skip === "undefined" ? [] : skip.slice(0);
+	skip.sort(function (a, b) { return a - b; }).push(-1);
+	let skipInd = 0;
+
+	// Start with all vertex normals as <0,0,0,0>
+	for (let i = 0; i < verts.length; i++) { normals[i] = vec4(0, 0, 0, 0); }
+
+	// Calculate the face normals for each triangle then add them to the vertices
+	let inc = strip ? 1 : 3;
+	for (let i = 0; i < inds.length - 2; i+=inc) {
+		if (i === skip[skipInd]) { skipInd++; continue; } // skip this triangle
+		let j = inds[i], k = inds[i+1], l = inds[i+2];
+		if (j === k || k === l || l === j) { continue; } // degenerate triangle, skip it
+		let a = ensure_vec4(verts[j], 1), b = ensure_vec4(verts[k], 1), c = ensure_vec4(verts[l], 1);
+		let face_norm = cross((strip && (i%2) !== 0) ? subtract(a, b) : subtract(b, a), subtract(a, c));
+		normals[j] = add(normals[j], face_norm);
+		normals[k] = add(normals[k], face_norm);
+		normals[l] = add(normals[l], face_norm);
+	}
+
+	// Normalize the normals
+	for (let i = 0; i < verts.length; i++) { normals[i] = normalize(normals[i]); }
+	return normals;
+}
+
+/**
+ * Generate a mesh of triangles from a 2D dataset that indicates the height of the mesh at every
+ * point. The result is to be drawn with gl.drawElements(gl.TRIANGLE_STRIP).
+ */
+function generate_mesh(data, verts, inds) {
+	let off = verts.length, n = data.length, m = data[0].length;
+	// Just the vertices
+	for (let i = 0; i < n; i++) {
+		for (let j = 0; j < m; j++) {
+			verts.push(vec4(2*i/(n-1)-1, data[i][j], 2*j/(m-1)-1, 1.0));
+		}
+	}
+	// Rectangles
+	for (let i = 0; i < n-1; i++) {
+		for (let j = 0; j < m; j++) {
+			inds.push(off+i*m+j);
+			inds.push(off+(i+1)*m+j);
+		}
+		inds.push(off+(i+2)*m-1);
+		inds.push(off+(i+1)*m);
+	}
+}
+
+/**
+ * Ensures that the argument v is a vec4 with the given last value. If it is only a vec3 than the
+ * last value is appended and it is returned.
+ */
+function ensure_vec4(v, last) {
+	if (v.length === 3) {
+		v = vec4(v, last);
+	} else if (v.length !== 4 || v[3] !== last) { throw "invalid argument value"; }
+	return v;
+}
+
+
+///////////// Shapes /////////////
+
+/**
+ * Adds a cube to verts/inds defined by the vertices a, b, c, d, e, f, g, h with
+ * abcd and efgh as opposite faces of the cube.
+ * The indices need to be drawn as a triangle strip.
+ */
+function cube(a, b, c, d, e, f, g, h, verts, inds) {
+	let off = verts.length;
+	verts.push(a, b, c, d, e, f, g, h);
+	inds.push(off+0, off+2,
+		off+3, off+4, off+0, // around vertex d
+		off+7, off+6, off+4, // around vertex h
+		off+5, off+2, off+6, // around vertex f
+		off+1, off+0, off+2  // around vertex b
+	);
+}
+
+/**
+ * Adds a tetrahedron to verts/inds defined by the vertices a, b, c, and d.
+ * The indices need to be drawn as a triangle strip.
+ */
+function tetrahedron(a, b, c, d, verts, inds) {
+	let off = verts.length;
+	verts.push(a, b, c, d);
+	inds.push(off+0, off+1, off+2, off+3, off+0, off+1);
+}
+
+/**
+ * Create an approximate sphere with the given radius using vec3s.
+ */
+function sphere(r, verts, inds, num_subdivisions) {
+	// The unit sphere has 4 equidistant points at:
+	//    <0,0,-1>, <0,2*sqrt(2)/3,1/3>, <-sqrt(6)/3, -sqrt(2)/3, 1/3>, and <sqrt(6)/3, -sqrt(2)/3, 1/3>
+	let a = vec3(0.0, 0.0, -1.0);
+	let b = vec3(0.0, 0.94280904158, 0.33333333333);
+	let c = vec3(-0.81649658093, -0.4714045207, 0.33333333333);
+	let d = vec3( 0.81649658093, -0.4714045207, 0.33333333333);
+	let map = new Map();
+	let verts_start = verts.length;
+	divide_triangle(a, b, c, verts, inds, map, num_subdivisions);
+	divide_triangle(d, c, b, verts, inds, map, num_subdivisions);
+	divide_triangle(a, d, b, verts, inds, map, num_subdivisions);
+	divide_triangle(a, c, d, verts, inds, map, num_subdivisions);
+	for (let i = verts_start; i < verts.length; i++) { verts[i] = scale(r, normalize(verts[i])); }
+}
+
+/**
+ * Create an approximate unit sphere by subdividing the faces of a tetrahedron repeatedly. The
+ * sphere will be centered at the origin and have a radius of 1. For different spheres the
+ * vertices can just be transformed as necessary.
+ */
+function unit_sphere(verts, inds, num_subdivisions) {
+	// The unit sphere has 4 equidistant points at:
+	//    <0,0,-1>, <0,2*sqrt(2)/3,1/3>, <-sqrt(6)/3, -sqrt(2)/3, 1/3>, and <sqrt(6)/3, -sqrt(2)/3, 1/3>
+	let a = vec4(0.0, 0.0, -1.0, 1);
+	let b = vec4(0.0, 0.94280904158, 0.33333333333, 1);
+	let c = vec4(-0.81649658093, -0.4714045207, 0.33333333333, 1);
+	let d = vec4( 0.81649658093, -0.4714045207, 0.33333333333, 1);
+	let map = new Map();
+	let verts_start = verts.length;
+	divide_triangle(a, b, c, verts, inds, map, num_subdivisions);
+	divide_triangle(d, c, b, verts, inds, map, num_subdivisions);
+	divide_triangle(a, d, b, verts, inds, map, num_subdivisions);
+	divide_triangle(a, c, d, verts, inds, map, num_subdivisions);
+	for (let i = verts_start; i < verts.length; i++) { verts[i] = normalize(verts[i], true); }
+}
+
+/**
+ * Divide a triangle into several triangles.
+ */
+function divide_triangle(a, b, c, verts, inds, map, num_subdivisions) {
+	if (num_subdivisions === 0) {
+		inds.push(
+			get_vert_idx(a, verts, map),
+			get_vert_idx(b, verts, map),
+			get_vert_idx(c, verts, map));
+	} else {
+		let ab = mix(a, b, 0.5);
+		let ac = mix(a, c, 0.5);
+		let bc = mix(b, c, 0.5);
+		divide_triangle(a, ab, ac, verts, inds, map, num_subdivisions-1);
+		divide_triangle(ab, b, bc, verts, inds, map, num_subdivisions-1);
+		divide_triangle(bc, c, ac, verts, inds, map, num_subdivisions-1);
+		divide_triangle(ab, bc, ac, verts, inds, map, num_subdivisions-1);
+	}
+}
+
+/**
+ * Gets the index of the vertex v. If v is already in verts/map than its previous index is returned
+ * otherwise v is added to verts and map and its new index is returned.
+ */
+function get_vert_idx(v, verts, map) {
+	let str = v.toString();
+	if (!map.has(str)) { map.set(str, verts.length); verts.push(v); }
+	return map.get(str);
+}
